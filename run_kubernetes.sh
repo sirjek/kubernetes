902d5646ffae823ed72a49fd5d167c465e80bc91
Eugene Kadiri <eugenejcase@gmail.com>
2020-04-26 17:22:39 +0200

    add new

---
+++ run_kubernetes.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=eugenek19/api:latest
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run newapp --image=$dockerpath --port=8080
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward deployment/newapp 8080:80
+
